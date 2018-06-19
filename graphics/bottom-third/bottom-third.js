cd77e47e5d405b20031f7e873600f00d111e1867
Jai Sharma <jai.p.sharma1@gmail.com>
2018-06-19 17:28:18 +0100

    improve lower third graphic

--- graphics/bottom-third/bottom-third.js
+++ graphics/bottom-third/bottom-third.js
@@ -2,14 +2,40 @@
 
 const interview = nodecg.Replicant("interview");
 
+const makeTwitchIcon = () => {
+	const icon = document.createElement("img");
+	icon.setAttribute("src", "../img/twitch-icon.svg");
+	icon.classList.add("twitch-icon");
+	return icon;
+};
+
+const makeDivText = (text) => {
+	const node = document.createElement("div");
+	const txt = document.createTextNode(text);
+	node.appendChild(txt);
+	return node;
+};
+
+const makeDivWithClass = (cls) => {
+	const div = document.createElement("div");
+	div.classList.add(cls);
+	return div;
+};
+
 interview.on("change", (newVal, oldVal) => {
-	const container = document.getElementById('bottom-third-container');
+	const container = document.getElementById("bottom-third-container");
 	container.innerHTML = null;
 
 	for(const person of newVal) {
-		const banner = document.createElement('div');
-		banner.classList.add('banner');
-		banner.appendChild(document.createTextNode(person.name));
+		const banner = makeDivWithClass("banner");
+		banner.appendChild(makeDivText(person.name));
+		// Only add twitch div if a twitch name is present
+		if (person.twitch) {
+			const twitch = makeDivWithClass("twitch");
+			twitch.appendChild(makeTwitchIcon());
+			twitch.appendChild(makeDivText(person.twitch));
+			banner.appendChild(twitch);
+		}
 		container.appendChild(banner);
 	}
 });
